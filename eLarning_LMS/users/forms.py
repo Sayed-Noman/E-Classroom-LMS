ab6b5abe926407a9df004cc5718c88cc926f89a4
Sayed Noman <Sayed.siddik@northsouth.edu>
2021-03-25 23:14:06 +0600

    user registration form added

---
+++ eLarning_LMS/users/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.models import User
+from users.models import user_profile
+from django.contrib.auth.forms import UserCreationForm, User
+
+
+class UserForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta():
+        model = User
+        fields = ('username','first_name','last_name','email','password1','password2')
+
+        labels = {
+            'password1': 'Password',
+            'password2': 'Confirm Password',
+        }
+
+class UserProfileInfoForm(forms.ModelForm):
+    bio = forms.CharField(required=False)
+    teacher ='teacher'
+    student = 'student'
+    parent = 'parent'
+
+    user_types = [
+        (student, 'student'),
+        (parent, 'parent'),
+    ]
+
+    user_type = forms.CharField(required=True)
+
+    class Meta():
+        model = user_profile
+        fields = ('bio','profile_pic','user_type')
--- eLarning_LMS/users/views.py
+++ eLarning_LMS/users/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from users.forms import UserProfileInfoForm,UserForm
 
 def index(request):
     return HttpResponse('This is Homepage')
+
+
+def register(request):
+    registered  = False
+
+    if register.methode == 'POST':
+        user_form = UserForm(data = request.POST)
+        profile_form = UserProfileInfoForm(data= request.POST)
+
+        if user_form.is_valid() and profile_form.is_valid() :
+            user = user_form.save()
+            user.save()
+
+            profile = profile_form.save(commit= False)
+            profile.user = user
+            profile.save()
+
+            registered = True
+
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form =UserForm()
+        profile_form = UserProfileInfoForm()
+    
+
+    context ={
+        'registered': registered,
+        'user_form': user_form,
+        'profile_form': profile_form,
+    }
+    
+    return render(request, 'users/registration.html', context)
